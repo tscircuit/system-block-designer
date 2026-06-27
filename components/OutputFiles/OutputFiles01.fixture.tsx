0a0f19ac085faec65cd56108b20e9521291cf85c
imrishabh18 <belikerishabh@gmail.com>
2026-06-27 17:20:20 +0530

    Add the Output Files view

---
+++ components/OutputFiles/OutputFiles01.fixture.tsx
@@ -0,0 +1,3 @@
+import { OutputFiles01Page } from "./OutputFiles01.page"
+
+export default <OutputFiles01Page />
---
+++ components/OutputFiles/OutputFiles01.page.tsx
@@ -0,0 +1,7 @@
+import { OutputFiles } from "./OutputFiles"
+
+export function OutputFiles01Page() {
+  return <OutputFiles />
+}
+
+export default OutputFiles01Page
