b13f2fc11c8047ba777f0dc3ba13636351055c67
imrishabh18 <belikerishabh@gmail.com>
2026-06-27 19:17:10 +0530

    updt id

--- tests/system-json-single-block-system.test.ts
+++ tests/system-json-single-block-system.test.ts
@@ -6,13 +6,13 @@ import { systemJsonToSvgSnapshot } from "./fixtures/system-json-to-svg-snapshot"
 const singleBlockSystem: SystemJson[] = [
   {
     type: "system_diagram",
-    system_diagram_id: "diagram_single_block",
+    system_diagram_id: "system_diagram_0",
     name: "Single Block",
   },
   {
     type: "system_block",
-    system_diagram_id: "diagram_single_block",
-    system_block_id: "sensor",
+    system_diagram_id: "system_diagram_0",
+    system_block_id: "system_block_0",
     center: { x: 100, y: 100 },
     size: { width: 128, height: 104 },
     label: "Sensor",
@@ -21,9 +21,9 @@ const singleBlockSystem: SystemJson[] = [
   },
   {
     type: "system_port",
-    system_diagram_id: "diagram_single_block",
-    system_port_id: "sensor_r_0",
-    system_block_id: "sensor",
+    system_diagram_id: "system_diagram_0",
+    system_port_id: "system_port_0",
+    system_block_id: "system_block_0",
     label: "OUT",
     side_of_block: "right",
   },
