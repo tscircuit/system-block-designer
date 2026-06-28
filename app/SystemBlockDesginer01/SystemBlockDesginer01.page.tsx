e6ba7780826ec95fedd33f8dc9d54bb9affee851
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2026-06-29 00:18:42 +0530

    Export tsx zip contains all the subcircuit's and connections getting endited (#18)

---
+++ app/SystemBlockDesginer01/SystemBlockDesginer01.page.tsx
@@ -0,0 +1,33 @@
+import { SystemBlockDesigner } from "../../components/SystemBlockDesigner/SystemBlockDesigner"
+import type { SystemJson } from "../../lib/system-json/system-json"
+import { createSystemBlockDesginer01SystemJson } from "./createSystemBlockDesginer01SystemJson"
+
+interface SystemBlockDesginer01PageProps {
+  debug?: boolean
+  initialSystemJson?: SystemJson[]
+}
+
+export function SystemBlockDesginer01Page({
+  debug = false,
+  initialSystemJson = createSystemBlockDesginer01SystemJson(),
+}: SystemBlockDesginer01PageProps) {
+  return (
+    <SystemBlockDesigner
+      projectTitle="System Block Desginer 01"
+      initialSystemJson={initialSystemJson}
+      debugOptions={
+        debug
+          ? {
+              showSystemJsonDownload: true,
+              systemJsonDownloadFilename: "system-block-desginer-01.json",
+              showCircuitJsonDownload: true,
+              circuitJsonDownloadFilename:
+                "system-block-desginer-01-circuit.json",
+            }
+          : undefined
+      }
+    />
+  )
+}
+
+export default SystemBlockDesginer01Page
